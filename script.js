2b9f1aff8d6c6eb9bfc31e861117fceb5e05f889
Conor <67439130+conorkenahan@users.noreply.github.com>
2021-02-21 13:06:33 -0500

    fetch call with sorting added

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const API_URL = 'https://starwars.egghead.training/';
+const output = document.getElementById("output");
 
-const responsePromise = fetch(API_URL + 'films');
-responsePromise.then(res => {
-    return res.json();
-}).then(films => {
-    console.log(films)
+output.innerText = "Loading...";
+
+fetch(API_URL + 'films')
+ .then(res => res.json())
+ .then(films => {
+     console.log(films);
+     output.innerText = getFilmTitles(films);
 })
+
+function getFilmTitles(films) {
+    return films
+    .sort((a,b) => a.episode_id - b.episode_id)
+    .map(film => `${film.episode_id}. ${film.title}`)
+    .join("\n")
+}
