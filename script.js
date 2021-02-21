7e265f38a2a9dcba5ca7424b167d1ef046dd7ec3
Conor <67439130+conorkenahan@users.noreply.github.com>
2021-02-21 12:55:00 -0500

    fetch and promises added

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const API_URL = 'https://starwars.egghead.training/';
 
 const responsePromise = fetch(API_URL + 'films');
-responsePromise.then(response => {
-    console.log(response);
+responsePromise.then(res => {
+    return res.json();
+}).then(films => {
+    console.log(films)
 })
